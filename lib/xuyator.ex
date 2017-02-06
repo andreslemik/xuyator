94923722b0c86899151491c0a2e65b48445c861a
Andres <andreslemik@yandex.ru>
2017-02-06 15:05:11 +0400

    fix

--- lib/xuyator.ex
+++ lib/xuyator.ex
@@ -1,33 +1,33 @@
 defmodule Xuyator do
   @stopwords ["через", "что", "вокруг", "перед", "возле", "ввиду"]
-    @replaces %{"о" => "е", "а" => "я", "у" => "ю", "ы" => "и"}
+  @replaces %{"о" => "е", "а" => "я", "у" => "ю", "ы" => "и"}
 
-    def vxuyarim(sentence) do
-      String.split(sentence)
-      |> Enum.reduce(fn(x, acc) -> acc <> " " <> xuyator(x) end)
-    end
+  def vxuyarim(sentence) do
+    String.split(sentence)
+    |> Enum.reduce(fn(x, acc) -> acc <> " " <> xuyator(x) end)
+  end
 
-    def xuyator(word) do
-      if (String.length(word) <= 3) or (Enum.member?(@stopwords, String.downcase(word))) do
-        word
-      else
-        word <> postfix(String.slice(word, -5..-1))
-      end
+  def xuyator(word) do
+    if (String.length(word) <= 3) or (Enum.member?(@stopwords, String.downcase(word))) do
+      word
+    else
+      word <> postfix(String.slice(word, -5..-1))
     end
+  end
 
-    defp postfix(xuy) do
-      list = xuy
-        |> String.downcase
-        |> strip
-        |> String.graphemes
-      if list == [] do
-        ""
-      else
-        "-ху" <> ([Map.get(@replaces, hd(list), hd(list))] ++ tl(list) |> List.to_string)
-      end
+  defp postfix(xuy) do
+    list = xuy
+      |> String.downcase
+      |> strip
+      |> String.graphemes
+    if list == [] do
+      ""
+    else
+      "-ху" <> ([Map.get(@replaces, hd(list), hd(list))] ++ tl(list) |> List.to_string)
     end
+  end
 
-    defp strip(word) do
-      Regex.replace(~r/^[бвгджзйклмнпрстфхцчшщьъ]+/u, word, "")
-    end
+  defp strip(word) do
+    Regex.replace(~r/^[бвгджзйклмнпрстфхцчшщьъ]+/u, word, "")
+  end
 end
